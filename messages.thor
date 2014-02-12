49ea8e69a833b0eb1ac6e2cd78d8466512f38c42
tatums <tatum@ashlandstudios.com>
2014-02-11 18:50:47 -0600

    add wip

---
+++ messages.thor
@@ -0,0 +1,21 @@
+require 'telegram'
+require 'pry'
+
+class Messages < Thor
+  desc "pending", "read pending messages"
+  def pending
+
+    system("clear")
+    messages = Message.all
+
+    messages.each_with_index do |m, index|
+      puts "#{index+1}:\t#{m.created_at.strftime("%m/%d/%Y")} - #{m.body}"
+      print "\tAcknowledged by:"
+      m.acknowledgments.each {|a|
+        puts "#{a.user}"
+      }
+
+    end
+
+  end
+end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -5,33 +5,38 @@ describe Message do
     before(:each) do
       Dir.stub(:glob).and_return(['spec/fixtures/example_1.yml'])
     end
-    let(:messages) {Message.all}
+    let(:messages) { Message.all }
     it 'should return an array' do
       expect(messages).to be_an_instance_of(Array)
     end
   end
 
-
   describe '#save' do
-
-    let(:file) { double(File, write: true, close: nil ) }
+    let(:file) { double(File, write: true, close: nil) }
 
     context "When a message with the body of test" do
-
       let(:message) { Message.new(body: 'test') }
-
       it 'should write a file and merge in additional keys' do
         File.should_receive(:open).and_return(file)
+
         file.should_receive(:write).with do |args|
           options = YAML::load(args)
-          expect(options.keys).to include(:body)
-          expect(options.keys).to include(:user)
-          expect(options.keys).to include(:acknowledgments)
-          expect(options.keys).to include(:created_at)
+          [:body, :user, :acknowledgments, :created_at].each do |item|
+            expect(options.keys).to include(item)
+          end
         end
         message.save
       end
+    end
 
+    describe '#acknowledge' do
+      before(:each) do
+        Dir.stub(:glob).and_return(['spec/fixtures/example_1.yml'])
+      end
+      let(:message) { Message.all.first }
+      it 'should NOT change the created_at date' do
+        expect{ message.ack }.not_to change{ message.created_at }
+      end
     end
 
   end
