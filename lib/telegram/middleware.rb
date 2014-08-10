940015c60653984fa653362fbb666a92a90721cf
tatums <tatum@ashlandstudios.com>
2014-08-10 08:49:28 -0500

    move stop middleware

--- lib/telegram/middleware.rb
+++ lib/telegram/middleware.rb
@@ -3,6 +3,7 @@ require 'rack/response'
 module Telegram
   class Middleware
     def initialize(app)
+      pending_message_stop
       @app = app
     end
 
@@ -38,48 +39,57 @@ module Telegram
 
     private
 
-    def body(items)
-      <<-HTML
-        <html>
-          <head>
-            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/united/bootstrap.min.css">
-            <style>
-                body{
-                    background-color: #E6E6E6;
-                    margin:0.5em;
-                    }
+      def pending_message_stop
+        messages = Message.not_acknowledged
+        if messages.any?
+          raise PendingMessageError.new(messages.length)
+          exit!
+        end
+      end
 
-                ul{ margin:0;
-                    padding:0;
-                }
 
-                ul li {
-                    margin: 0.5em 0;
-                    list-style: none;
-                    padding: 0.7em;
-                    background: #fff;
-                    -webkit-border-radius: 3px;
-                    -moz-border-radius: 3px;
-                    border-radius: 3px;
-                    -webkit-box-shadow: 0 1px 2px rgba(0,0,0,0.07);
-                    -moz-box-shadow: 0 1px 2px rgba(0,0,0,0.07);
-                    box-shadow: 0 1px 2px rgba(0,0,0,0.07);
-                }
-            </style>
-          </head>
-          <body>
-          <h1>You have pending messages.</h1>
-          <ul>
-            #{items}
-          </ul>
-          <div>
-            <p>You can clear these messages using Telegram from the terminal.</p>
-            <p><a href="https://github.com/tatums/telegram">View the read me on Github.</a>
-          </div>
-        </body>
-        </html>
-      HTML
-    end
+      def body(items)
+        <<-HTML
+          <html>
+            <head>
+              <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/united/bootstrap.min.css">
+              <style>
+                  body{
+                      background-color: #E6E6E6;
+                      margin:0.5em;
+                      }
+
+                  ul{ margin:0;
+                      padding:0;
+                  }
+
+                  ul li {
+                      margin: 0.5em 0;
+                      list-style: none;
+                      padding: 0.7em;
+                      background: #fff;
+                      -webkit-border-radius: 3px;
+                      -moz-border-radius: 3px;
+                      border-radius: 3px;
+                      -webkit-box-shadow: 0 1px 2px rgba(0,0,0,0.07);
+                      -moz-box-shadow: 0 1px 2px rgba(0,0,0,0.07);
+                      box-shadow: 0 1px 2px rgba(0,0,0,0.07);
+                  }
+              </style>
+            </head>
+            <body>
+            <h1>You have pending messages.</h1>
+            <ul>
+              #{items}
+            </ul>
+            <div>
+              <p>You can clear these messages using Telegram from the terminal.</p>
+              <p><a href="https://github.com/tatums/telegram">View the read me on Github.</a>
+            </div>
+          </body>
+          </html>
+        HTML
+      end
 
   end
 end
